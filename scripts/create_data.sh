fcd6b721888dce867ca8c23d4d69effbf2a8b82b
ahmeddawy <ahmed94dawy@gmail.com>
2025-10-19 10:28:07 +0300

    Restore test

--- scripts/create_data.sh
+++ scripts/create_data.sh
@@ -1,16 +1,16 @@
 export PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
 
 python tools/data_converter/nuscenes_converter.py nuscenes \
-    --root-path ./data/nuscenes \
-    --canbus ./data/nuscenes \
+    --root-path ./data/nuscenes_mini \
+    --canbus ./data/nuscenes_mini \
     --out-dir ./data/infos/ \
     --extra-tag nuscenes \
     --version v1.0-mini
 
 # python tools/data_converter/nuscenes_converter.py nuscenes \
-#     --root-path /home/oem/Practice/PHD/nuscenes_full/v1.0-trainval \
-#     --canbus  /home/oem/Practice/PHD/nuscenes_full/v1.0-trainval \
-#     --out-dir  /home/oem/Practice/PHD/nuscenes_full/infos/ \
+#     --root-path ./data/nuscenes \
+#     --canbus  ./data/nuscenes \
+#     --out-dir  ./data/infos/ \
 #     --extra-tag nuscenes \
 #     --version v1.0
 
